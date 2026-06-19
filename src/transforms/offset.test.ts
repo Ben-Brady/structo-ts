6c9d6f8c00cf31901bf5be1b391d38d81c86b321
Ben Brady <benbrady04.bb@gmail.com>
2026-06-19 18:06:59 +0100

    feat: reworked offset

---
+++ src/transforms/offset.test.ts
@@ -0,0 +1,72 @@
+import { describe, it } from "bun:test";
+import { encodeTest } from "../utils.test";
+
+import * as st from "../index";
+
+describe("st.offset(relative)", () => {
+    it("encodes correctly with fixed offset", () => {
+        const spec = st.object({
+            foo: st.u32(),
+            bar: st.pipe(
+                st.u32(),
+                st.offset("relative", -4), //
+            ),
+        });
+        encodeTest(
+            spec,
+            { foo: 1, bar: 5 },
+            { foo: 5, bar: 5 }, //
+        );
+    });
+
+    it("encodes correctly with read offset", () => {
+        const offset = st.createRememberedValue<number>();
+        const spec = st.object({
+            foo: st.u32(),
+            offset: offset.save(st.s16()),
+            bar: st.pipe(
+                st.u32(),
+                st.offset("relative", offset.load()), //
+            ),
+        });
+        encodeTest(
+            spec,
+            { foo: 0, offset: -6, bar: 5 },
+            { foo: 5, offset: -6, bar: 5 }, //
+        );
+    });
+});
+
+describe("st.offset(absolute)", () => {
+    it("encodes correctly with fixed offset", () => {
+        const spec = st.object({
+            foo: st.u32(),
+            bar: st.pipe(
+                st.u32(),
+                st.offset("absolute", 0), //
+            ),
+        });
+        encodeTest(
+            spec,
+            { foo: 0, bar: 5 },
+            { foo: 5, bar: 5 }, //
+        );
+    });
+
+    it("encodes correctly with read offset", () => {
+        const offset = st.createRememberedValue<number>();
+        const spec = st.object({
+            foo: st.u32(),
+            offset: offset.save(st.u32()),
+            bar: st.pipe(
+                st.u32(),
+                st.offset("absolute", offset.load()), //
+            ),
+        });
+        encodeTest(
+            spec,
+            { foo: 0, offset: 0, bar: 5 },
+            { foo: 5, offset: 0, bar: 5 }, //
+        );
+    });
+});
---
+++ src/transforms/offset.ts
@@ -0,0 +1,29 @@
+import type { Serializer } from "../types";
+
+export function offset<TIn, TOut>(
+    behaviour: "relative" | "absolute",
+    offset: Serializer<number> | number,
+) {
+    return (type: Serializer<TIn, TOut>): Serializer<TIn, TOut> => ({
+        size: type.size,
+        read: (ctx) => {
+            let start = ctx.offset;
+
+            const offsetValue = typeof offset === "number" ? offset : offset.read(ctx);
+            ctx.offset = behaviour === "absolute" ? offsetValue : ctx.offset + offsetValue;
+
+            const value = type.read(ctx);
+            ctx.offset = start;
+            return value;
+        },
+        write: (ctx, value) => {
+            let start = ctx.offset;
+
+            const offsetValue = typeof offset === "number" ? offset : offset.read(ctx);
+            ctx.offset = behaviour === "absolute" ? offsetValue : ctx.offset + offsetValue;
+
+            type.write(ctx, value);
+            ctx.offset = start;
+        },
+    });
+}
